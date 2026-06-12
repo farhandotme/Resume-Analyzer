ffdea20666bce0523bee97c21db07d123ff97c67
farhandotme <princeofficial355@gmail.com>
2026-06-12 23:58:21 +0530

    password route

--- app_backend/src/routes/userRoutes.ts
+++ app_backend/src/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import {
   login,
   logout,
   profile,
+  setPassword,
   verifyEmail,
   verifyOtp,
 } from "../controllers/userControllers";
@@ -12,6 +13,7 @@ const router = Router();
 
 router.post("/verify-email", verifyEmail);
 router.post("/verify-otp", verifyOtp);
+router.post("/set-password", setPassword);
 router.post("/login", login);
 router.post("/profile", verifyToken, profile);
 router.get("/logout", logout);
