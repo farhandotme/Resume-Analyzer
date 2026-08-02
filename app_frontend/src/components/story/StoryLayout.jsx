63a013fa2df2a37863d227a66249f2286e5e09b5
Farid Hussain <faridhussain0011@gmail.com>
2026-08-02 12:18:06 +0530

    remove frontend to rebuild new product experience

--- app_frontend/src/components/story/StoryLayout.jsx
+++
@@ -1,7 +0,0 @@
-export default function StoryLayout({ children }) {
-    return (
-        <main className='relative h-screen w-screen overflow-hidden bg-neutral-950 text-white'>
-            { children }
-        </main>
-    )
-}
